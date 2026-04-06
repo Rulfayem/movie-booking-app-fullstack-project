53ae0178431f5def7bc3f044ac8cb911e4caf437
alfaim <hassan.alfaim@gmail.com>
2026-04-06 22:53:12 +0800

    first commit, putting project on github

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+function HomePage() {
+    return (
+        <div>
+            <h1>Movie Seat Booking</h1>
+            <p>Temporary stuff</p>
+        </div>
+    );
+}
+
+export default HomePage;
