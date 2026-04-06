3f0dcde2f1c93f12ae8839aaa5e6f26ddfedc237
alfaim <hassan.alfaim@gmail.com>
2026-04-06 23:27:48 +0800

    added navbar and fixed some spacing index.css stuff

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,40 @@
+import { Navbar, Container, Button } from "react-bootstrap";
+import logoImage from "../assets/movie-coding-logo.png";
+
 function HomePage() {
+    const siteLogo = logoImage;
+    const siteTitle = "Alfaim's Awe-Inspiring Movie Multiplex!";
+
     return (
         <div>
-            <h1>Movie Seat Booking</h1>
-            <p>Temporary stuff</p>
+            {/* navbar stuff */}
+            <Navbar bg="dark" variant="dark">
+                <Container fluid className="d-flex justify-content-between">
+                    <div className="d-flex align-items-center">
+                        <img
+                            src={siteLogo}
+                            alt="logo"
+                            style={{ width: "40px", marginRight: "10px" }}
+                        />
+                        <Navbar.Brand>{siteTitle}</Navbar.Brand>
+                    </div>
+                    <div>
+                        <Button variant="outline-light" className="me-2">
+                            Login
+                        </Button>
+                        <Button variant="light">
+                            Sign Up
+                        </Button>
+                    </div>
+
+                </Container>
+            </Navbar>
+
+            {/* main page content stuff */}
+            <div style={{ padding: "20px" }}>
+                <h1>Movie Seat Booking</h1>
+                <p>Temporary stuff</p>
+            </div>
         </div>
     );
 }
